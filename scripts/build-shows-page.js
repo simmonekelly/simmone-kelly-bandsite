f051c6e705c383c780c057e52faa5a41870b7ba5
Simmone Kelly <simmone.kelly@gmail.com>
2021-12-18 16:30:37 -0500

    adding highlighting row function, converting arrays to axios, adding post function to form but does not append array yet

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,11 +1,4 @@
-let showList = [
-{date: "Mon Sept 06 2021", venue: "Ronald Lane", location: "San Francisco, CA"},
-{date: "Tue Sept 21 2021", venue: "Pier 3 East", location: "San Francisco, CA"},
-{date: "Fri Oct 15 2021", venue: "View Lounge", location: "San Francisco, CA"}, 
-{date: "Sat Nov 06 2021", venue: "Hyatt Agency", location: "San Francisco, CA"}, 
-{date: "Fri Nov 26 2021", venue: "Moscow Center",location: "San Francisco, CA"}, 
-{date: "Wed Dec 15 2021", venue: "Press Club", location: "San Francisco, CA"}
-]
+let api = '6ceb11dc-2ff7-4333-a7b2-e59085b2da97'
 
 //creates table
 let getParent = document.querySelector('.shows');
@@ -16,7 +9,6 @@ getParent.appendChild(createShowTable)
 //creates desktop/table show row
 let createShowRow = document.createElement('tr');
 createShowRow.classList.add('desktop_show_row')
-console.log(createShowRow)
 createShowTable.appendChild(createShowRow)
 
 //creates desktop/table date header
@@ -24,14 +16,12 @@ let createDateHeader = document.createElement ('th')
 createDateHeader.classList.add('desktop_show_date_header')
 createDateHeader.innerText = 'Date';
 createShowRow.appendChild(createDateHeader)
-console.log(createDateHeader.innerText)
 
 //creates desktop/table venue header
 let createVenueHeader = document.createElement ('th')
 createVenueHeader.classList.add('desktop_show_venue_header')
 createVenueHeader.innerText = 'Venue';
 createShowRow.appendChild(createVenueHeader)
-console.log(createVenueHeader.innerText)
 
 //create desktop/table location headr
 let createLocationHeader = document.createElement ('th')
@@ -40,90 +30,118 @@ createLocationHeader.innerText = 'Location';
 createShowRow.appendChild(createLocationHeader)
 
 // create desktop/tablet button blank header
-
 let createButtonHeader = document.createElement ('th')
 createButtonHeader.classList.add('desktop_show_button_header')
 createButtonHeader.innerText = '';
 createShowRow.appendChild(createButtonHeader)
 
-
-for (let i = 0; i < showList.length; i++) {
-//creates show row
-let createShowRow = document.createElement('tr');
-createShowRow.classList.add('show_row')
-console.log(createShowRow)
-createShowTable.appendChild(createShowRow)
-
-//creates date header
-let createDateHeader = document.createElement ('th')
-createDateHeader.classList.add('show_date_header')
-createDateHeader.innerText = 'Date';
-createShowRow.appendChild(createDateHeader)
-console.log(createDateHeader.innerText)
-
-//adds date
-let addShowDate = document.createElement('td');
-addShowDate.classList.add('show_date');
-addShowDate.innerText = showList[i].date;
-createShowRow.appendChild(addShowDate)
-console.log(addShowDate.innerText)
-
-//creates venue header
-let createVenueHeader = document.createElement ('th')
-createVenueHeader.classList.add('show_venue_header')
-createVenueHeader.innerText = 'Venue';
-createShowRow.appendChild(createVenueHeader)
-console.log(createVenueHeader.innerText)
-
-//add venue
-let addShowVenue = document.createElement ('td')
-addShowVenue.classList.add('show_venue')
-addShowVenue.innerText = showList[i].venue;
-createShowRow.appendChild(addShowVenue)
-console.log(addShowVenue.innerText)
-
-//create location headr
-let createLocationHeader = document.createElement ('th')
-createLocationHeader.classList.add('show_location_header')
-createLocationHeader.innerText = 'Location';
-createShowRow.appendChild(createLocationHeader)
-
-//add locaition
-let addShowLocation = document.createElement ('td')
-addShowLocation.classList.add('show_location')
-addShowLocation.innerText = showList[i].location;
-createShowRow.appendChild(addShowLocation)
-console.log(addShowLocation.innerText)
-
-// create button blank header
-
-let createButtonHeader = document.createElement ('th')
-createButtonHeader.classList.add('show_button_header')
-createButtonHeader.innerText = '';
-createShowRow.appendChild(createButtonHeader)
-
-//create button section
-let createShowButton = document.createElement ('td')
-createShowButton.classList.add('show_button_section')
-createShowRow.appendChild(createShowButton)
-
-// add button
-let addButton = document.createElement('button')
-addButton.classList.add('show_button')
-addButton.innerText = 'Buy Tickets';
-createShowButton.appendChild(addButton)
-console.log(addButton.innerText)
-
-//add line
-let createLine = document.createElement('hr')
-createLine.classList.add('show_line_break')
-createShowRow.appendChild(createLine)
+//format date
+function formatDate(dateString) {
+    let date = new Date(dateString)
+    return (date.getUTCMonth() + 1) + '/' + date.getUTCDate() + '/' + date.getUTCFullYear();
 }
 
-// adding active class on click i couldnt figure this out :/
-// createShowTable.addEventListener('click', fucntion(event) { //add event listener to click
-//     const showClick = event.target, //even target
-// })
-// let selectShowRow = document.querySelector('.show_row'); //selecting the show row
-// selectShowRow.classList.add('active') //adding active class to show row
+//function to create rows
+function createShowList(showDetails) {
+    showDetails.forEach(show => {
+        //creates show row
+        let createShowRow = document.createElement('tr');
+        createShowRow.classList.add('show_row')
+        createShowTable.appendChild(createShowRow)
+
+        //creates date header
+        let createDateHeader = document.createElement ('th')
+        createDateHeader.classList.add('show_date_header')
+        createDateHeader.innerText = 'Date';
+        createShowRow.appendChild(createDateHeader)
+
+        //adds date
+        let addShowDate = document.createElement('td');
+        addShowDate.classList.add('show_date');
+        addShowDate.innerText = formatDate(show.date);
+        createShowRow.appendChild(addShowDate)
+
+        //creates venue header
+        let createVenueHeader = document.createElement ('th')
+        createVenueHeader.classList.add('show_venue_header')
+        createVenueHeader.innerText = 'Venue';
+        createShowRow.appendChild(createVenueHeader)
+
+        //add venue
+        let addShowVenue = document.createElement ('td')
+        addShowVenue.classList.add('show_venue')
+        addShowVenue.innerText = show.place;
+        createShowRow.appendChild(addShowVenue)
+
+        //create location headr
+        let createLocationHeader = document.createElement ('th')
+        createLocationHeader.classList.add('show_location_header')
+        createLocationHeader.innerText = 'Location';
+        createShowRow.appendChild(createLocationHeader)
+
+        //add locaition
+        let addShowLocation = document.createElement ('td')
+        addShowLocation.classList.add('show_location')
+        addShowLocation.innerText = show.location;
+        createShowRow.appendChild(addShowLocation)
+
+        // create button blank header
+        let createButtonHeader = document.createElement ('th')
+        createButtonHeader.classList.add('show_button_header')
+        createButtonHeader.innerText = '';
+        createShowRow.appendChild(createButtonHeader)
+
+        //create button section
+        let createShowButton = document.createElement ('td')
+        createShowButton.classList.add('show_button_section')
+        createShowRow.appendChild(createShowButton)
+
+        // add button
+        let addButton = document.createElement('button')
+        addButton.classList.add('show_button')
+        addButton.innerText = 'Buy Tickets';
+        createShowButton.appendChild(addButton)
+
+        //add line
+        let createLine = document.createElement('hr')
+        createLine.classList.add('show_line_break')
+        createShowRow.appendChild(createLine)
+    })
+}
+
+//highlighting row when selected function
+//need to fix if statement
+function highlightShowRow() {
+    const selectShowRow = document.querySelectorAll('.show_row')
+    selectShowRow.forEach(row => {
+        row.addEventListener('click', (event) =>{
+            if (row.classList.contains('active')){
+                row.classList.remove('active')
+            }
+            else {
+                row.classList.add('active')
+            }
+        })
+    })
+}
+
+
+//axios
+const p = axios.get('https://project-1-api.herokuapp.com/showdates?api_key=api');
+p.then(results => {
+    console.log(results.data)
+
+    //run show list function
+    createShowList(results.data)
+
+    //run highlight function
+    highlightShowRow()
+
+})
+.catch( error => {
+    console.log(error)
+})
+
+
+
 
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,107 +1,288 @@
+let api = '6ceb11dc-2ff7-4333-a7b2-e59085b2da97'
+
 let commentList = [
     {user_name: "Connor Walton", date: new Date("02/17/2021"), comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."} ,
     {user_name: "Emilie Beach", date: new Date("01/09/2021"), comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."},
-    {user_name: "Miles Acosta", date: new Date("12/20/2020"), comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."}]
+    {user_name: "Miles Acosta", date: new Date("12/20/2020"), comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."}
+]
+
 let getCommentContainer = document.querySelector('.posted-comments')
 
-//format date
-function formatDate(date) {
-    return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
-}
+// //format date
+// function formatDate(date) {
+//     return (date.getUTCMonth() + 1) + '/' + date.getUTCDate() + '/' + date.getUTCFullYear();
+// }
 
-// creates comment section
-function displayComment() {
+// // creates comment section
+// function displayComment() {
 
-    //sort comments
-    const sortedComments = commentList.sort((a,b) => {
-        return b.date - a.date;
-    })
+//     //sort comments
+//     const sortedComments = commentList.sort((a,b) => {
+//         return b.date - a.date;
+//     })
 
-for (let i = 0; i < sortedComments.length; i++) {
+//     sortedComments.forEach((comment) => {
 
-//creates comment div
-let createCommentSection = document.createElement('div')
-createCommentSection.classList.add('posted-comments_container')
-console.log(getCommentContainer)
-getCommentContainer.appendChild(createCommentSection)
+//         //creates comment div
+//         let createCommentSection = document.createElement('div')
+//         createCommentSection.classList.add('posted-comments_container')
+//         getCommentContainer.appendChild(createCommentSection)
 
-//creates avatar
-let createAvatar = document.createElement('figure')
-createAvatar.classList.add('posted-comments_avatar')
-createCommentSection.appendChild(createAvatar)
+//         //creates avatar
+//         let createAvatar = document.createElement('figure')
+//         createAvatar.classList.add('posted-comments_avatar')
+//         createCommentSection.appendChild(createAvatar)
 
-//creates comment div
-let createCommentDiv = document.createElement('div')
-createCommentDiv.classList.add('posted-comments_section')
-console.log(getCommentContainer)
-createCommentSection.appendChild(createCommentDiv)
+//         //creates comment div
+//         let createCommentDiv = document.createElement('div')
+//         createCommentDiv.classList.add('posted-comments_section')
+//         createCommentSection.appendChild(createCommentDiv)
 
-//creates header div
-let createHeaderSection = document.createElement('div')
-createHeaderSection.classList.add('posted-comments_header-seciton')
-console.log(getCommentContainer)
-createCommentDiv.appendChild(createHeaderSection)
+//         //creates header div
+//         let createHeaderSection = document.createElement('div')
+//         createHeaderSection.classList.add('posted-comments_header-seciton')
+//         createCommentDiv.appendChild(createHeaderSection)
 
-//creates name header
-let createNameHeader = document.createElement('p')
-createNameHeader.classList.add('posted-comments_header-name')
-createNameHeader.innerText = commentList[i].user_name;
-createHeaderSection.appendChild(createNameHeader)
-console.log(createNameHeader.innerText)
+//         //creates name header
+//         let createNameHeader = document.createElement('p')
+//         createNameHeader.classList.add('posted-comments_header-name')
+//         createNameHeader.innerText = comment.user_name;
+//         createHeaderSection.appendChild(createNameHeader)
 
-//creates date section
-let createDate = document.createElement('p')
-createDate.classList.add('posted-comments_header-date')
-createDate.innerText = formatDate(commentList[i].date);
-createHeaderSection.appendChild(createDate)
-console.log(createDate.innerText)
+//         //creates date section
+//         let createDate = document.createElement('p')
+//         createDate.classList.add('posted-comments_header-date')
+//         createDate.innerText = formatDate(comment.date);
+//         createHeaderSection.appendChild(createDate)
 
-//creates comment section
-let createComment = document.createElement('p')
-createComment.classList.add('posted-comments_comment')
-createComment.innerText = commentList[i].comment;
-createCommentDiv.appendChild(createComment)
-console.log(createComment.innerText)
+//         //creates comment section
+//         let createComment = document.createElement('p')
+//         createComment.classList.add('posted-comments_comment')
+//         createComment.innerText = comment.comment;
+//         createCommentDiv.appendChild(createComment)
 
-//creates line
-let createLine = document.createElement('hr')
-createLine.classList.add('comment_seperator')
-getCommentContainer.appendChild(createLine)
-}
-}
+//         //creates line
+//         let createLine = document.createElement('hr')
+//         createLine.classList.add('comment_seperator')
+//         getCommentContainer.appendChild(createLine)
+//     })
+// }
 
+// //loads comment section on pageload
+// window.addEventListener('load',displayComment)
+
+// //form submission function
+// const commentForm = document.getElementById('comments-form');
+// commentForm.addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     const formTarget = event.target;
+//     commentList.push(
+//         {user_name: formTarget.user_name.value,
+//         date: new Date(Date.now()),
+//         comment: formTarget.comment.value})
+//     clearComments();
+//     displayComment();
+//     commentForm.reset();
+// })
+
+
+
+// //Clears all comments from the page
+// function clearComments() {
+//     let getCommentSection = document.querySelector('.posted-comments')
+//     let commentToRemove = document.querySelectorAll('.posted-comments_container')
+//     let lineToRemove = document.querySelectorAll('.comment_seperator')
+    
+//     commentToRemove.forEach(comment => {
+//         getCommentSection.removeChild(comment)
+//     })
+
+//     lineToRemove.forEach(line => {
+//         getCommentSection.removeChild(line)
+//     })
+// }
+
+
+// //loads comment section on pageload
+// window.addEventListener('load',displayComment)
+
+
+
+//NEW CODE
 //loads comment section on pageload
 window.addEventListener('load',displayComment)
+//format date
+function formatDate(dateString) {
+    let date = new Date(dateString);
+    return (date.getUTCMonth() + 1) + '/' + date.getUTCDate() + '/' + date.getUTCFullYear();
+}
+
+//function to load comments
+function loadComments(commentList) {
+    commentList.forEach((comment) => {
+        //creates comment div
+        let createCommentSection = document.createElement('div')
+        createCommentSection.classList.add('posted-comments_container')
+        getCommentContainer.appendChild(createCommentSection)
+
+        //creates avatar
+        let createAvatar = document.createElement('figure')
+        createAvatar.classList.add('posted-comments_avatar')
+        createCommentSection.appendChild(createAvatar)
+
+        //creates comment div
+        let createCommentDiv = document.createElement('div')
+        createCommentDiv.classList.add('posted-comments_section')
+        createCommentSection.appendChild(createCommentDiv)
+
+        //creates header div
+        let createHeaderSection = document.createElement('div')
+        createHeaderSection.classList.add('posted-comments_header-seciton')
+        createCommentDiv.appendChild(createHeaderSection)
+
+        //creates name header
+        let createNameHeader = document.createElement('p')
+        createNameHeader.classList.add('posted-comments_header-name')
+        createNameHeader.innerText = comment.name;
+        createHeaderSection.appendChild(createNameHeader)
+
+        //creates date section
+        let createDate = document.createElement('p')
+        createDate.classList.add('posted-comments_header-date')
+        createDate.innerText = formatDate(comment.timestamp);
+        createHeaderSection.appendChild(createDate)
+
+        //creates comment section
+        let createComment = document.createElement('p')
+        createComment.classList.add('posted-comments_comment')
+        createComment.innerText = comment.comment;
+        createCommentDiv.appendChild(createComment)
+
+        //creates line
+        let createLine = document.createElement('hr')
+        createLine.classList.add('comment_seperator')
+        getCommentContainer.appendChild(createLine)
+    })
+}
+
+// function to dipslay comments
+function displayComment() {
+
+    //axios
+    const p = axios.get('https://project-1-api.herokuapp.com/comments?api_key=api');
+    p.then(results => {
+        console.log(results.data)
+
+        let originalComments = results.data
+        
+        //sort comments
+        const sortedComments = originalComments.sort((a,b) => {
+                    return b.date - a.date;
+                })
+
+        loadComments(sortedComments);
+        //submitForm();
+    })
+    .catch(error => {
+    console.log(error)
+    })
+
+}
+
+//what is the difference between creating an event listener to post the comment vs setting the action
+//on the form to the URL and the method on the form  to post 
+
+
+// steps for form
+// on click of button post comment to axios
+// push to current array results.data
+// clear comments
+// reload comment array
+// do we have to recall the get method since it previously was in another function?
 
 //form submission function
-const commentForm = document.getElementById('comments-form');
-commentForm.addEventListener('submit', function(event) {
+// not loading since other funcitn is on load
+// function submitForm() {
+//     const commentForm = document.getElementById('comments_form')
+//     console.log(commentForm)
+//     commentForm.addEventListener('submit', event => {
+//         event.preventDefault();
+//         console.log('Comment Submitted')
+//         const formTarget = event.target
+//         const name = formTarget.user_name.value
+//         const comment = formTarget.comment.value
+//         postComment(name,comment)
+//         //clearComments();
+//         //displayComment();
+//         commentForm.reset();
+//     })
+// }
+
+
+const commentForm = document.getElementById('comments-form')
+commentForm.addEventListener('submit', event => {
     event.preventDefault();
     console.log('Comment Submitted')
-    const formTarget = event.target;
-    console.log(formTarget.user_name.value);
-    console.log(commentList[commentList.length - 1]);
-    commentList.push(
-        {user_name: formTarget.user_name.value,
-        date: new Date(Date.now()),
-        comment: formTarget.comment.value})
-    console.log(commentList[commentList.length - 1]);
+    const formTarget = event.target
+    const name = formTarget.user_name.value
+    const comment = formTarget.comment.value
+    postComment(name,comment)
     clearComments();
     displayComment();
-    console.log(commentList[commentList.length]);
     commentForm.reset();
 })
+
+
+
+//posting new comment to axios
+function postComment(passedName, passedComment) {
+    const p = axios.post('https://project-1-api.herokuapp.com/comments?api_key=api', {
+        name: passedName,
+        comment: passedComment
+    });
+    p.then(results => {
+        console.log(results.data)
+        //need to append new array here
+        //let newComment = results.form
+        //console.log(newComment)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+// //form submission function need to add post functionality to this
+// const commentForm = document.getElementById('comments-form');
+// commentForm.addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     console.log('Comment Submitted')
+//     const formTarget = event.target;
+//     console.log(formTarget.user_name.value);
+//     console.log(commentList[commentList.length - 1]);
+//     commentList.push(
+//         {user_name: formTarget.user_name.value,
+//         date: new Date(Date.now()),
+//         comment: formTarget.comment.value})
+//     console.log(commentList[commentList.length - 1]);
+//     clearComments();
+//     displayComment();
+//     console.log(commentList[commentList.length]);
+//     commentForm.reset();
+// })
+
+
 
 // Clears all comments from the page
 function clearComments() {
     let getCommentSection = document.querySelector('.posted-comments')
     let commentToRemove = document.querySelectorAll('.posted-comments_container')
     let lineToRemove = document.querySelectorAll('.comment_seperator')
-    //commentToRemove.forEach()[getCommentSection.remove()]
-    for (let i = 0; i < (commentToRemove.length); i++) {
-        getCommentSection.removeChild(commentToRemove[i])
-    }
-    for (let i = 0; i < (lineToRemove.length); i++) {
-        getCommentSection.removeChild(lineToRemove[i])
-    }
+
+    commentToRemove.forEach(comment => {
+        getCommentSection.removeChild(comment)
+    })
+
+    lineToRemove.forEach(line => {
+        getCommentSection.removeChild(line)
+    })
+
 }
