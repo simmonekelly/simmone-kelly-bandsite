6b023f9240612ed5e5adb220d9f84414d75d6db9
Simmone Kelly <simmone.kelly@gmail.com>
2021-12-18 18:54:32 -0500

    updated comment sort and fixed post comment call to display new comment without refreshing

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,11 +1,5 @@
 let api = '6ceb11dc-2ff7-4333-a7b2-e59085b2da97'
 
-let commentList = [
-    {user_name: "Connor Walton", date: new Date("02/17/2021"), comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."} ,
-    {user_name: "Emilie Beach", date: new Date("01/09/2021"), comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."},
-    {user_name: "Miles Acosta", date: new Date("12/20/2020"), comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."}
-]
-
 let getCommentContainer = document.querySelector('.posted-comments')
 
 // //format date
@@ -21,96 +15,9 @@ let getCommentContainer = document.querySelector('.posted-comments')
 //         return b.date - a.date;
 //     })
 
-//     sortedComments.forEach((comment) => {
-
-//         //creates comment div
-//         let createCommentSection = document.createElement('div')
-//         createCommentSection.classList.add('posted-comments_container')
-//         getCommentContainer.appendChild(createCommentSection)
-
-//         //creates avatar
-//         let createAvatar = document.createElement('figure')
-//         createAvatar.classList.add('posted-comments_avatar')
-//         createCommentSection.appendChild(createAvatar)
-
-//         //creates comment div
-//         let createCommentDiv = document.createElement('div')
-//         createCommentDiv.classList.add('posted-comments_section')
-//         createCommentSection.appendChild(createCommentDiv)
-
-//         //creates header div
-//         let createHeaderSection = document.createElement('div')
-//         createHeaderSection.classList.add('posted-comments_header-seciton')
-//         createCommentDiv.appendChild(createHeaderSection)
-
-//         //creates name header
-//         let createNameHeader = document.createElement('p')
-//         createNameHeader.classList.add('posted-comments_header-name')
-//         createNameHeader.innerText = comment.user_name;
-//         createHeaderSection.appendChild(createNameHeader)
-
-//         //creates date section
-//         let createDate = document.createElement('p')
-//         createDate.classList.add('posted-comments_header-date')
-//         createDate.innerText = formatDate(comment.date);
-//         createHeaderSection.appendChild(createDate)
-
-//         //creates comment section
-//         let createComment = document.createElement('p')
-//         createComment.classList.add('posted-comments_comment')
-//         createComment.innerText = comment.comment;
-//         createCommentDiv.appendChild(createComment)
-
-//         //creates line
-//         let createLine = document.createElement('hr')
-//         createLine.classList.add('comment_seperator')
-//         getCommentContainer.appendChild(createLine)
-//     })
-// }
-
-// //loads comment section on pageload
-// window.addEventListener('load',displayComment)
-
-// //form submission function
-// const commentForm = document.getElementById('comments-form');
-// commentForm.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     const formTarget = event.target;
-//     commentList.push(
-//         {user_name: formTarget.user_name.value,
-//         date: new Date(Date.now()),
-//         comment: formTarget.comment.value})
-//     clearComments();
-//     displayComment();
-//     commentForm.reset();
-// })
-
-
-
-// //Clears all comments from the page
-// function clearComments() {
-//     let getCommentSection = document.querySelector('.posted-comments')
-//     let commentToRemove = document.querySelectorAll('.posted-comments_container')
-//     let lineToRemove = document.querySelectorAll('.comment_seperator')
-    
-//     commentToRemove.forEach(comment => {
-//         getCommentSection.removeChild(comment)
-//     })
-
-//     lineToRemove.forEach(line => {
-//         getCommentSection.removeChild(line)
-//     })
-// }
-
-
-// //loads comment section on pageload
-// window.addEventListener('load',displayComment)
-
-
-
-//NEW CODE
 //loads comment section on pageload
 window.addEventListener('load',displayComment)
+
 //format date
 function formatDate(dateString) {
     let date = new Date(dateString);
@@ -168,20 +75,18 @@ function loadComments(commentList) {
 // function to dipslay comments
 function displayComment() {
 
-    //axios
-    const p = axios.get('https://project-1-api.herokuapp.com/comments?api_key=api');
+    //axios get request
+    const p = axios.get('https://project-1-api.herokuapp.com/comments?api_key=' + api);
     p.then(results => {
         console.log(results.data)
-
-        let originalComments = results.data
+        let comments = results.data
         
         //sort comments
-        const sortedComments = originalComments.sort((a,b) => {
-                    return b.date - a.date;
-                })
+        const sortedComments = comments.sort((a,b) => {
+            return b.timestamp - a.timestamp;
+        })
 
         loadComments(sortedComments);
-        //submitForm();
     })
     .catch(error => {
     console.log(error)
@@ -189,46 +94,16 @@ function displayComment() {
 
 }
 
-//what is the difference between creating an event listener to post the comment vs setting the action
-//on the form to the URL and the method on the form  to post 
-
-
-// steps for form
-// on click of button post comment to axios
-// push to current array results.data
-// clear comments
-// reload comment array
-// do we have to recall the get method since it previously was in another function?
-
-//form submission function
-// not loading since other funcitn is on load
-// function submitForm() {
-//     const commentForm = document.getElementById('comments_form')
-//     console.log(commentForm)
-//     commentForm.addEventListener('submit', event => {
-//         event.preventDefault();
-//         console.log('Comment Submitted')
-//         const formTarget = event.target
-//         const name = formTarget.user_name.value
-//         const comment = formTarget.comment.value
-//         postComment(name,comment)
-//         //clearComments();
-//         //displayComment();
-//         commentForm.reset();
-//     })
-// }
-
-
+// event listener for submitting form
 const commentForm = document.getElementById('comments-form')
 commentForm.addEventListener('submit', event => {
     event.preventDefault();
     console.log('Comment Submitted')
     const formTarget = event.target
-    const name = formTarget.user_name.value
-    const comment = formTarget.comment.value
-    postComment(name,comment)
+    const passedName = formTarget.user_name.value
+    const passedComment = formTarget.comment.value
+    postComment(passedName,passedComment)
     clearComments();
-    displayComment();
     commentForm.reset();
 })
 
@@ -236,40 +111,19 @@ commentForm.addEventListener('submit', event => {
 
 //posting new comment to axios
 function postComment(passedName, passedComment) {
-    const p = axios.post('https://project-1-api.herokuapp.com/comments?api_key=api', {
+
+    const newComment = axios.post('https://project-1-api.herokuapp.com/comments?api_key=' + api, {
         name: passedName,
         comment: passedComment
     });
-    p.then(results => {
+    newComment.then(results => {
         console.log(results.data)
-        //need to append new array here
-        //let newComment = results.form
-        //console.log(newComment)
+        displayComment();
     })
     .catch(error => {
         console.log(error)
     })
 }
-// //form submission function need to add post functionality to this
-// const commentForm = document.getElementById('comments-form');
-// commentForm.addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     console.log('Comment Submitted')
-//     const formTarget = event.target;
-//     console.log(formTarget.user_name.value);
-//     console.log(commentList[commentList.length - 1]);
-//     commentList.push(
-//         {user_name: formTarget.user_name.value,
-//         date: new Date(Date.now()),
-//         comment: formTarget.comment.value})
-//     console.log(commentList[commentList.length - 1]);
-//     clearComments();
-//     displayComment();
-//     console.log(commentList[commentList.length]);
-//     commentForm.reset();
-// })
-
-
 
 // Clears all comments from the page
 function clearComments() {
@@ -284,5 +138,4 @@ function clearComments() {
     lineToRemove.forEach(line => {
         getCommentSection.removeChild(line)
     })
-
 }
